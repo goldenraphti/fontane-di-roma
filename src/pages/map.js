5a1b4b24b93c12cb8b4338dd18eb7f4465cd9228
Raphaël Ferrand <ferrand@protonmail.ch>
2020-01-25 00:05:28 +0100

    added leaflet map plugin (working)

--- src/pages/map.js
+++ src/pages/map.js
@@ -3,12 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import MapUI from "../components/mapui"
 
 const MapPage = () => (
   <Layout>
     <SEO title="Contact" />
     <h1>Map</h1>
     <p>Welcome to Map Page</p>
+    <MapUI
+      style={{
+        width: `600px`,
+        height: `400px`,
+      }}
+     />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
