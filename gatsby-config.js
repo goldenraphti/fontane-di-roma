f8d7593f6df4f51e278dead88bf965ca271f937d
Raphaël Ferrand <ferrand@protonmail.ch>
2020-03-15 17:34:39 +0100

    fix purgecss whitelist mapui.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         develop: false, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['/mapui.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     },
